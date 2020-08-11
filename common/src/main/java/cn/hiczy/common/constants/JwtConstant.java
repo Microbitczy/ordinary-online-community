566ce37d3dc98f9738fc4379a4717237e9b222da
czy77 <czy_git@163.com>
2020-08-11 23:55:42 +0800

    新增JWT工具类

--- common/src/main/java/cn/hiczy/common/constants/JwtConstant.java
+++ common/src/main/java/cn/hiczy/common/constants/JwtConstant.java
@@ -2,6 +2,19 @@ package cn.hiczy.common.constants;
 
 public class JwtConstant {
 
+    /*密钥*/
     public static final String JWT_SECRET = "secret1234";
+    /*签发人*/
+    public static final String ISSUER = "bbs";
+
+
+    /*以下是自定义的claims信息*/
+    /*当前用户角色信息*/
+    public static final String ROLES = "roles";
+    /*当前用户所拥有的权限*/
+    public static final String PERMISSIONS = "permissions";
+
+
+
 
 }
